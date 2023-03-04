bf6d36783e429d49e3b9eaa474969938434889a7
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-03-04 01:56:17 +0100

    Work at colors

--- KnitterNotebook/Themes/ThemeChanger.cs
+++ KnitterNotebook/Themes/ThemeChanger.cs
@@ -11,22 +11,14 @@ namespace KnitterNotebook.Themes
         /// <summary>
         /// Sets current theme
         /// </summary>
-        /// <param name="resourceDictionaryFullName">Full path of resource dictionary of selected theme, like DefaultMode.xaml</param>
+        /// <param name="resourceDictionaryFullName">Full path of resource dictionary of selected theme, like c:\..\DefaultMode.xaml</param>
         public static void SetTheme(string resourceDictionaryFullName)
         {
             try
             {
-               // App.Current.Resources.Clear();
-                //App.Current.Resources.MergedDictionaries.Clear();
-                //App.Current.Resources.Source = new Uri($"/AppThemes/{ReadTheme()}.xaml", UriKind.Relative);
-                
                 //Gets current theme
                 var result = App.Current.Resources.MergedDictionaries.FirstOrDefault(x => x.Source.ToString().Contains("Mode.xaml"));
                 App.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri(resourceDictionaryFullName) });
-               
-                MessageBox.Show(result.ToString());
-               // App.Current.Resources.MergedDictionaries.Remove(DarkMode.xaml);
-               // App.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri($"/AppThemes/{ReadTheme()}.xaml", UriKind.Relative) });
             }
             catch (Exception exception)
             {
