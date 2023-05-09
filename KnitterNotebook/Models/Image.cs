4d5e6a1f1a1edaccfb77d3c39f46e19c372edbeb
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-05-10 00:17:37 +0200

    Work at sample

---
+++ KnitterNotebook/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnitterNotebook.Models
+{
+    public class Image
+    {
+        public  int Id { get; set; }
+
+        public string Path { get; set; } = string.Empty;
+    }
+}
