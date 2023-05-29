7cb072a9e5d89946b5852507d0737be0495a90c8
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-05-29 23:40:41 +0200

    Start working at PlannedProject

---
+++ KnitterNotebook/Models/PlannedProject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace KnitterNotebook.Models
+{
+    public class PlannedProject
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string YarnName { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+       // public virtual List<Needle> Needles { get; set; } = new();
+
+       // public virtual List<Yarn> Yarns { get; set; } = new();
+    }
+}
