93dd1843d82ee76de417c55e713681a3c095b7e1
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-07-09 16:08:02 +0200

    Testing user registration

--- KnitterNotebook/Validators/RegisterUserDtoValidator.cs
+++ KnitterNotebook/Validators/RegisterUserDtoValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using KnitterNotebook.Database;
 using KnitterNotebook.Models.Dtos;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace KnitterNotebook.Validators
@@ -16,40 +17,24 @@ namespace KnitterNotebook.Validators
             RuleFor(x => x.Nickname)
                 .NotEmpty().WithMessage("Nazwa użytkownika nie może być pusta")
                 .MinimumLength(1).WithMessage("Nazwa użytkownika musi mieć conajmniej 1 znak")
-                .MaximumLength(50).WithMessage("Nazwa użytkownika może mieć maksimum 50 znaków");
+                .MaximumLength(50).WithMessage("Nazwa użytkownika może mieć maksimum 50 znaków")
+                .MustAsync(async (value, cancellationToken) => await _databaseContext.Users.AllAsync(x => x.Nickname != value, cancellationToken))
+                .WithMessage("Nick jest już używany");
 
             RuleFor(x => x.Email)
                 .NotEmpty().WithMessage("E-mail nie może być pusty")
                 .MaximumLength(50).WithMessage("E-mail może mieć maksimum 50 znaków")
-                .EmailAddress().WithMessage("Niepoprawny format e-mail");
+                .EmailAddress().WithMessage("Niepoprawny format e-mail")
+                .MustAsync(async (value, cancellationToken) => await _databaseContext.Users.AllAsync(x => x.Email != value, cancellationToken))
+                .WithMessage("E-mail jest już używany");
 
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("Hasło nie może być puste")
                 .MinimumLength(6).WithMessage("Hasło musi mieć conajmniej 6 znaków")
                 .MaximumLength(50).WithMessage("Hasło może mieć maksimum 50 znaków")
-                .Must(y => y.Any(char.IsDigit)).WithMessage("Hasło musi zawierać conajmniej jedną cyfrę")
-                .Must(y => y.Any(char.IsAsciiLetterLower)).WithMessage("Hasło musi zawierać conajmniej jedną małą literę")
-                .Must(y => y.Any(char.IsAsciiLetterUpper)).WithMessage("Hasło musi zawierać conajmniej jedną wielką literę");
-
-            RuleFor(x => x.Nickname)
-               .Custom((value, context) =>
-               {
-                   bool isNicknameUsed = _databaseContext.Users.Any(x => x.Nickname == value);
-                   if (isNicknameUsed)
-                   {
-                       context.AddFailure(nameof(RegisterUserDto.Nickname), "Nick jest już używany");
-                   }
-               });
-
-            RuleFor(x => x.Email)
-                .Custom((value, context) =>
-                {
-                    bool isEmailUsed = _databaseContext.Users.Any(x => x.Email == value);
-                    if (isEmailUsed)
-                    {
-                        context.AddFailure(nameof(RegisterUserDto.Email), "E-mail jest już używany");
-                    }
-                });
+                .Must(y => y is not null && y.Any(char.IsDigit)).WithMessage("Hasło musi zawierać conajmniej jedną cyfrę")
+                .Must(y => y is not null && y.Any(char.IsAsciiLetterLower)).WithMessage("Hasło musi zawierać conajmniej jedną małą literę")
+                .Must(y => y is not null && y.Any(char.IsAsciiLetterUpper)).WithMessage("Hasło musi zawierać conajmniej jedną wielką literę");
         }
     }
 }
---
+++ KnitterNotebookTests/Validators/RegisterUserDtoValidatorTests.cs
@@ -0,0 +1,92 @@
+﻿using KnitterNotebook.Database;
+using KnitterNotebook.Models.Dtos;
+using KnitterNotebook.Models;
+using KnitterNotebook.Validators;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+
+namespace KnitterNotebookTests.Validators
+{
+    public class RegisterUserDtoValidatorTests
+    {
+        private readonly RegisterUserDtoValidator _validator;
+        private readonly DatabaseContext _databaseContext;
+
+        public RegisterUserDtoValidatorTests()
+        {
+            DbContextOptionsBuilder<DatabaseContext> builder = new();
+            builder.UseInMemoryDatabase(CreateUniqueDatabaseName);
+            _databaseContext = new DatabaseContext(builder.Options);
+            _validator = new RegisterUserDtoValidator(_databaseContext);
+            SeedUsers();
+        }
+
+        public static IEnumerable<object[]> InvalidData()
+        {
+            yield return new object[] { new RegisterUserDto(null!, "valid@email.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("", "valid@email.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto(new string('K', 51), "valid@email.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto(new string('K', 51), "valid@email.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Nick1", "valid@email.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Nick2", "valid@email.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", null!, "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "nick1@mail.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "nick2@mail.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", new string('K', 51) + "@mail.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "mail.com", "ValidPassword123@#") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "validemail@mail.com", "") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "validemail@mail.com", null!) };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "validemail@mail.com", new string('K',5)) };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "validemail@mail.com", "kkdsgkkK") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "validemail@mail.com", "KKDAS75645") };
+            yield return new object[] { new RegisterUserDto("Valid Nick", "validemail@mail.com", "6542345") };
+        }
+
+        public static IEnumerable<object[]> ValidData()
+        {
+            yield return new object[] { new RegisterUserDto("Valid Nickname", "uservalidemail@mail.com", "Pass123@word") };
+            yield return new object[] { new RegisterUserDto("Nickname", "emailuser@mail.com", "Strong321@xSd") };
+        }
+
+        private static string CreateUniqueDatabaseName => "TestDb" + DateTime.Now.Ticks.ToString();
+
+        private void SeedUsers()
+        {
+            List<User> users = new()
+            {
+                new User() { Id = 1, Email = "nick1@mail.com", Nickname = "Nick1"},
+                new User() { Id = 2, Email = "nick2@mail.com", Nickname = "Nick2"},
+            };
+            _databaseContext.Users.AddRange(users);
+            _databaseContext.SaveChanges();
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidData))]
+        public async Task ValidateAsync_ForInvalidData_FailValidation(RegisterUserDto registerUserDto)
+        {
+            //Act
+            var validationResult = await _validator.TestValidateAsync(registerUserDto);
+
+            //Assert
+            validationResult.ShouldHaveAnyValidationError();
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidData))]
+        public async Task ValidateAsync_ForValidData_PassValidation(RegisterUserDto registerUserDto)
+        {
+            //Act
+            var validationResult = await _validator.TestValidateAsync(registerUserDto);
+
+            //Assert
+            validationResult.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
