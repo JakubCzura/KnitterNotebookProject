26d7d2fbbf41163c701ee025464a8e6096b967d1
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-07-20 20:02:34 +0200

    Refactor FileExtensionValidator

--- KnitterNotebook/Validators/FileExtensionValidator.cs
+++ KnitterNotebook/Validators/FileExtensionValidator.cs
@@ -6,22 +6,14 @@ namespace KnitterNotebook.Validators
 {
     public class FileExtensionValidator
     {
-        public static bool IsPdf(string? filePath)
-        {
-            string[] validExtensions = { ".pdf" };
-            return IsExtensionValid(filePath, validExtensions);
-        }
+        private static readonly string[] validPdfExtensions = { ".pdf" };
+        private static readonly string[] validImageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
 
-        public static bool IsImage(string? filePath)
-        {
-            string[] validExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
-            return IsExtensionValid(filePath, validExtensions);
-        }
+        public static bool IsPdf(string? filePath) => IsExtensionValid(filePath, validPdfExtensions);
+
+        public static bool IsImage(string? filePath) => IsExtensionValid(filePath, validImageExtensions);
 
         private static bool IsExtensionValid(string? filePath, string[] allowedExtensions)
-        {
-            string? extension = Path.GetExtension(filePath);
-            return allowedExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase);
-        }
+            => allowedExtensions.Contains(Path.GetExtension(filePath), StringComparer.OrdinalIgnoreCase);
     }
 }
