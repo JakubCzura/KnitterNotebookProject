d988c241b880236a52b5f20279367f88dc8e11ec
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-07-20 20:26:55 +0200

    FileExtensionValidator fix

--- KnitterNotebook/Validators/FileExtensionValidator.cs
+++ KnitterNotebook/Validators/FileExtensionValidator.cs
@@ -6,14 +6,14 @@ namespace KnitterNotebook.Validators
 {
     public class FileExtensionValidator
     {
-        private static readonly string[] validPdfExtensions = { ".pdf" };
+        private static readonly string validPdfExtensions = ".pdf";
         private static readonly string[] validImageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
 
         public static bool IsPdf(string? filePath) => IsExtensionValid(filePath, validPdfExtensions);
 
         public static bool IsImage(string? filePath) => IsExtensionValid(filePath, validImageExtensions);
 
-        private static bool IsExtensionValid(string? filePath, string[] allowedExtensions)
+        private static bool IsExtensionValid(string? filePath, params string[] allowedExtensions)
             => allowedExtensions.Contains(Path.GetExtension(filePath), StringComparer.OrdinalIgnoreCase);
     }
 }
