22ba37951447281d3226ffab5a0924ac402f34df
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-09-23 13:54:30 +0200

    Fix inheritance in projects

--- KnitterNotebook/Models/Dtos/FinishedProjectDto.cs
+++ KnitterNotebook/Models/Dtos/FinishedProjectDto.cs
@@ -1,25 +1,14 @@
 ﻿using KnitterNotebook.Models.Entities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace KnitterNotebook.Models.Dtos;
 
-public class FinishedProjectDto : BasicProjectDto
+public class FinishedProjectDto : ProjectInProgressDto
 {
     public FinishedProjectDto(Project project) : base(project)
     {
         EndDate = project.EndDate;
-        Needles = project.Needles.Select(x => new NeedleDto(x)).ToList();
-        Yarns = project.Yarns.Select(x => new YarnDto(x)).ToList();
-        ProjectImages = project.ProjectImages.Select(x => new ProjectImageDto(x)).ToList();
     }
 
     public DateTime? EndDate { get; set; }
-
-    public List<NeedleDto> Needles { get; set; }
-
-    public List<YarnDto> Yarns { get; set; }
-
-    public List<ProjectImageDto> ProjectImages { get; set; }
 }
--- KnitterNotebook/Models/Dtos/ProjectInProgressDto.cs
+++ KnitterNotebook/Models/Dtos/ProjectInProgressDto.cs
@@ -4,18 +4,12 @@ using System.Linq;
 
 namespace KnitterNotebook.Models.Dtos;
 
-public class ProjectInProgressDto : BasicProjectDto
+public class ProjectInProgressDto : PlannedProjectDto
 {
     public ProjectInProgressDto(Project project) : base(project)
     {
-        Needles = project.Needles.Select(x => new NeedleDto(x)).ToList();
-        Yarns = project.Yarns.Select(x => new YarnDto(x)).ToList();
         ProjectImages = project.ProjectImages.Select(x => new ProjectImageDto(x)).ToList();
     }
-
-    public List<NeedleDto> Needles { get; set; }
-
-    public List<YarnDto> Yarns { get; set; }
 
     public List<ProjectImageDto> ProjectImages { get; set; }
 }
