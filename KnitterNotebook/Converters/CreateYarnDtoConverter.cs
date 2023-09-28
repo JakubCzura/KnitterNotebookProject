7f833652d406aeac71afa7e2d9192293ed7d55bf
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-09-28 16:23:39 +0200

    Simplify CreateYarnDtoConverter

--- KnitterNotebook/Converters/CreateYarnDtoConverter.cs
+++ KnitterNotebook/Converters/CreateYarnDtoConverter.cs
@@ -1,4 +1,5 @@
 ﻿using KnitterNotebook.Models.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,20 +14,7 @@ public class CreateYarnDtoConverter
     /// <param name="delimiter">Delimiter to separate each yarn's name</param>
     /// <returns>IEnumerable of converted <seealso cref="CreateYarnDto"/> for valid <paramref name="yarnsNames"/>, otherwise <see cref="Enumerable.Empty{CreateYarnDto}"/></returns>
     public static IEnumerable<CreateYarnDto> Convert(string yarnsNames, char delimiter = ',')
-    {
-        if (string.IsNullOrWhiteSpace(yarnsNames))
-        {
-            return Enumerable.Empty<CreateYarnDto>();
-        }
-
-        yarnsNames = yarnsNames.TrimEnd();
-
-        if (yarnsNames.EndsWith(delimiter))
-        {
-            yarnsNames = yarnsNames[..^1];
-        }
-
-        //Trim() is used to remove spaces after delimiter, for example " yarn1, yarn2,   yarn3 " -> "yarn1", "yarn2", "yarn3"
-        return yarnsNames.Split(delimiter).Select(x => new CreateYarnDto(x.Trim()));
-    }
+        => yarnsNames?.Split(delimiter, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                     .Select(x => new CreateYarnDto(x.Trim()))
+                     ?? Enumerable.Empty<CreateYarnDto>();
 }
