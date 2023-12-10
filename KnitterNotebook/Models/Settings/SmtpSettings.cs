9e5a673975151f031c0b204f1eb7f95a9b4e0f44
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2023-12-10 15:30:23 +0100

    SmtpSettings model for appsettings

---
+++ KnitterNotebook/Models/Settings/SmtpSettings.cs
@@ -0,0 +1,11 @@
+﻿using MailKit.Security;
+
+namespace KnitterNotebook.Models.Settings;
+
+public class SmtpSettings
+{
+    public static string SectionKey => "SmtpSettings";
+    public string Host { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public SecureSocketOptions SecureSocketOptions { get; set; }
+}
--- KnitterNotebook/Services/EmailService.cs
+++ KnitterNotebook/Services/EmailService.cs
@@ -21,6 +21,7 @@ public class EmailService(IConfiguration configuration) : IEmailService
         //I used it only for learning purposes and didn't send credentials to GitHub
         //Email and password are not stored in appsettings.json so sending emails will not work when you clone this repository
         EmailSettings emailSettings = _configuration.GetSection(EmailSettings.SectionKey).Get<EmailSettings>()!;
+        SmtpSettings smtpSettings = _configuration.GetSection(SmtpSettings.SectionKey).Get<SmtpSettings>()!;
 
         MimeMessage email = new()
         {
@@ -31,7 +32,7 @@ public class EmailService(IConfiguration configuration) : IEmailService
         email.Body = new TextPart(TextFormat.Html) { Text = sendEmailDto.Body };
 
         using SmtpClient smtp = new();
-        await smtp.ConnectAsync("smtp-mail.outlook.com", 587, SecureSocketOptions.StartTls);
+        await smtp.ConnectAsync(smtpSettings.Host, smtpSettings.Port, smtpSettings.SecureSocketOptions);
         await smtp.AuthenticateAsync(emailSettings.Email, emailSettings.Password);
         await smtp.SendAsync(email);
         await smtp.DisconnectAsync(true);
