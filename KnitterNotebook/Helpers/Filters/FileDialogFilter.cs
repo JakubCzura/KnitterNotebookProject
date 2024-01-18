36fbdfa9a86e4cfd4a1c868857d6f955930c21d1
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2024-01-19 00:03:37 +0100

    Document code

--- KnitterNotebook/Helpers/Filters/FileDialogFilter.cs
+++ KnitterNotebook/Helpers/Filters/FileDialogFilter.cs
@@ -2,7 +2,13 @@
 
 public static class FileDialogFilter
 {
+    /// <summary>
+    /// Filter to allow only image files
+    /// </summary>
     public const string ImageFilter = "Image Files (*.jpg; *.jpeg; *.png)|*.jpg; *.jpeg; *.png";
 
+    /// <summary>
+    /// Filter to allow only pdf files
+    /// </summary>
     public const string PdfFilter = "Pdf Files (*.pdf)|*.pdf";
 }
