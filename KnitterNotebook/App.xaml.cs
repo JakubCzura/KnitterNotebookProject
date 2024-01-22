74a732870028c02fba9f2cdb9ab7e3c35adb9b50
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2024-01-22 20:10:19 +0100

    Extension method for registering validators

--- KnitterNotebook/App.xaml.cs
+++ KnitterNotebook/App.xaml.cs
@@ -48,21 +48,7 @@ public partial class App : Application
 
             #region Validators
 
-            services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
-            services.AddScoped<IValidator<ChangeNicknameDto>, ChangeNicknameDtoValidator>();
-            services.AddScoped<IValidator<ChangeEmailDto>, ChangeEmailDtoValidator>();
-            services.AddScoped<IValidator<ChangePasswordDto>, ChangePasswordDtoValidator>();
-            services.AddScoped<IValidator<ChangeThemeDto>, ChangeThemeDtoValidator>();
-            services.AddScoped<IValidator<CreateMovieUrlDto>, CreateMovieUrlDtoValidator>();
-            services.AddScoped<IValidator<CreateSampleDto>, CreateSampleDtoValidator>();
-            services.AddScoped<IValidator<CreateYarnDto>, CreateYarnDtoValidator>();
-            services.AddScoped<IValidator<CreateNeedleDto>, CreateNeedleDtoValidator>();
-            services.AddScoped<IValidator<ResetPasswordDto>, ResetPasswordDtoValidator>();
-            services.AddScoped<IValidator<PlanProjectDto>, PlanProjectDtoValidator>();
-            services.AddScoped<IValidator<LogInDto>, LogInDtoValidator>();
-            services.AddScoped<IValidator<CreateProjectImageDto>, CreateProjectImageDtoValidator>();
-            services.AddScoped<IValidator<ChangeProjectStatusDto>, ChangeProjectStatusDtoValidator>();
-            services.AddScoped<IValidator<EditProjectDto>, EditProjectDtoValidator>();
+            services.RegisterValidators();
 
             #endregion Validators
 
---
+++ KnitterNotebook/Helpers/Extensions/ValidatorsRegistration.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using KnitterNotebook.Models.Dtos;
+using KnitterNotebook.Validators;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KnitterNotebook.Helpers.Extensions;
+
+public static class ValidatorsRegistration
+{
+    public static IServiceCollection RegisterValidators(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+        services.AddScoped<IValidator<ChangeNicknameDto>, ChangeNicknameDtoValidator>();
+        services.AddScoped<IValidator<ChangeEmailDto>, ChangeEmailDtoValidator>();
+        services.AddScoped<IValidator<ChangePasswordDto>, ChangePasswordDtoValidator>();
+        services.AddScoped<IValidator<ChangeThemeDto>, ChangeThemeDtoValidator>();
+        services.AddScoped<IValidator<CreateMovieUrlDto>, CreateMovieUrlDtoValidator>();
+        services.AddScoped<IValidator<CreateSampleDto>, CreateSampleDtoValidator>();
+        services.AddScoped<IValidator<CreateYarnDto>, CreateYarnDtoValidator>();
+        services.AddScoped<IValidator<CreateNeedleDto>, CreateNeedleDtoValidator>();
+        services.AddScoped<IValidator<ResetPasswordDto>, ResetPasswordDtoValidator>();
+        services.AddScoped<IValidator<PlanProjectDto>, PlanProjectDtoValidator>();
+        services.AddScoped<IValidator<LogInDto>, LogInDtoValidator>();
+        services.AddScoped<IValidator<CreateProjectImageDto>, CreateProjectImageDtoValidator>();
+        services.AddScoped<IValidator<ChangeProjectStatusDto>, ChangeProjectStatusDtoValidator>();
+        services.AddScoped<IValidator<EditProjectDto>, EditProjectDtoValidator>();
+
+        return services;
+    }
+}
